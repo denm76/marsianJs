e1fd09b86499fcc8e9ca379313ab5926343fc444
denm76 <denm76@gmail.com>
2021-10-09 13:31:22 +0300

    Реализована работа с ДОМ деревом, событиями отправки формы, нажатия , удаление и сортировка массива в объекте БД

--- js/script.js
+++ js/script.js
@@ -14,55 +14,126 @@
 
 'use strict';
 
-const movieDB = {
-    movies: [
-        "Логан",
-        "Лига справедливости",
-        "Ла-ла лэнд",
-        "Одержимость",
-        "Скотт Пилигрим против..."
-    ]
-};
-
-const adv = document.querySelectorAll('.promo__adv img'),
-      poster = document.querySelector('.promo__bg'), 
-      genre = poster.querySelector('.promo__genre'),
-      movieList = document.querySelector('.promo__interactive-list');   
-      
-      
-adv.forEach(element => {
-    element.remove();
-});
-genre.textContent = 'ДРАМА';
-poster.style.background = "url('img/bg.jpg') center center/cover no-repeat";
-movieDB.movies.sort();
-
-movieList.innerHTML = "";
-
-movieDB.movies.forEach((film, index)=>{
-    movieList.innerHTML += ` 
-        <li class="promo__interactive-item">${index+1}. ${film}
-            <div class="delete"></div>
-        </li>
-    `;
-});
-
-const btn = document.querySelector('button');
-let counter = 0;
-const showEventTarget = (e) => {
-    if(counter == 0){
-        e.preventDefault();
-        console.log(e.target);
-        counter++;
-    }
-    else{
-         btn.removeEventListener('click', showEventTarget);
-    }
-       
+document.addEventListener('DOMContentLoaded', ()=>{
+    const movieDB = {
+        movies: [
+            "Логан",
+            "Лига справедливости",
+            "Ла-ла лэнд",
+            "Одержимость",
+            "Скотт Пилигрим против..."
+        ]
+    };
     
-};
+    const adv = document.querySelectorAll('.promo__adv img'),
+          poster = document.querySelector('.promo__bg'), 
+          genre = poster.querySelector('.promo__genre'),
+          movieList = document.querySelector('.promo__interactive-list'),
+          addForm = document.querySelector('form.add'),  
+          addInput = addForm.querySelector('.adding__input'),
+          checkbox = addForm.querySelector('[type="checkbox"]');
+          
+    const checkFilmLenght = (film) => {
+        if(film.length > 21){
+            return film.slice(0, 21)+'...';
+        }
+        else{
+            return film;
+        }
+    };
+    
+   
 
-btn.addEventListener('click', showEventTarget);
+
+    const sortArray = (arr) =>{
+        arr.sort();
+    };
+
+    
+    
+    const deleteAdv = (arr) =>{
+            arr.forEach(element => {
+            element.remove();
+        });
+    };
+    
+    
+
+    const makeChanges =() =>{
+        genre.textContent = 'ДРАМА';
+        poster.style.backgroundImage = "url('img/bg.jpg')";
+    };
+
+    
+
+    function createMovieList(films, parent){
+            parent.innerHTML = "";
+            sortArray(films);
+            films.forEach((film, index)=>{
+                parent.innerHTML += ` 
+                    <li class="promo__interactive-item">
+                    ${index+1}. ${checkFilmLenght(film)}
+                        <div class="delete"></div>
+                    </li>
+                `;
+        });
+
+        document.querySelectorAll('.delete').forEach((btn, index)=>{
+            btn.addEventListener('click', ()=>{
+                btn.parentElement.remove();
+                movieDB.movies.splice(index, 1);
+                createMovieList(films, parent);
+            });
+        });
+    }
+
+    
+
+    sortArray(movieDB.movies);
+    deleteAdv(adv);
+    makeChanges();
+    createMovieList(movieDB.movies, movieList);
+    
+    addForm.addEventListener('submit', (event)=>{
+        event.preventDefault();
+        
+        const newFilm = addInput.value;
+        const favorite = checkbox.checked;
+        
+        if(favorite){
+            console.log('Добавляем любимый фильм');
+        }
+
+        if(newFilm){
+            movieDB.movies.push(newFilm);
+            sortArray(movieDB.movies);
+            createMovieList(movieDB.movies, movieList);
+            event.target.reset();
+        }
+       
+    });  
+    
+         
+            
+          
+    
+    // let counter = 0;
+    // const showEventTarget = (e) => {
+    //     if(counter == 0){
+    //         e.preventDefault();
+    //         console.log(e.target);
+    //         counter++;
+    //     }
+    //     else{
+    //          btn.removeEventListener('click', showEventTarget);
+    //     }
+           
+        
+    // };
+    
+    // btn.addEventListener('click', showEventTarget);
+});
+
 
 
     
